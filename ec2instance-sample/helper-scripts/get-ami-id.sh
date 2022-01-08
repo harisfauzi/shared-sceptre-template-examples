834089d96b3fcb00cbd7b055f7f31a37ad1c506a
Haris Fauzi <haris.fauzi@gmail.com>
2022-01-09 00:44:34 +1000

    Added ec2instance-sample

---
+++ ec2instance-sample/helper-scripts/get-ami-id.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+DEFAULT_IMAGE_PATTERN="amzn2-ami-hvm-2.?.????????.?-x86_64-ebs"
+IMAGE_PATTERN=""
+
+parse_arguments() {
+    while (( "$#" )); do
+      case "$1" in
+        -p|--pattern)
+          IMAGE_PATTERN=$2
+          shift 2
+          ;;
+      esac
+    done
+    # set positional arguments in their proper place
+    eval set -- "$PARAMS"
+}
+
+main() {
+  parse_arguments "$@"
+
+  if [ "z${IMAGE_PATTERN}" == "z" ]; then
+    IMAGE_PATTERN="${DEFAULT_IMAGE_PATTERN}"
+  fi
+  IMAGE_ID=$(aws ec2 describe-images --filters "Name=name,Values='${IMAGE_PATTERN}'" --owners amazon --query "reverse(sort_by(Images, &CreationDate))[0].ImageId" --output text)
+  echo "${IMAGE_ID}"
+}
+
+main "$@"
