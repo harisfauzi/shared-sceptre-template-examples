83308cc0843eaf7b48295b3a3c43fd2172943d5f
Haris Fauzi <haris.fauzi@gmail.com>
2022-01-11 23:04:18 +1000

    Made the script more flexible to get AMIs not owned by Amazon

--- ec2instance-sample/helper-scripts/get-ami-id.sh
+++ ec2instance-sample/helper-scripts/get-ami-id.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DEFAULT_IMAGE_PATTERN="amzn2-ami-hvm-2.?.????????.?-x86_64-ebs"
 IMAGE_PATTERN=""
+OWNER_ARG=""
 
 parse_arguments() {
     while (( "$#" )); do
@@ -8,6 +9,10 @@ parse_arguments() {
         -p|--pattern)
           IMAGE_PATTERN=$2
           shift 2
+          ;;
+        -o|--owner)
+          IMAGE_OWNER=$2
+          shit 2
           ;;
       esac
     done
@@ -21,7 +26,10 @@ main() {
   if [ "z${IMAGE_PATTERN}" == "z" ]; then
     IMAGE_PATTERN="${DEFAULT_IMAGE_PATTERN}"
   fi
-  IMAGE_ID=$(aws ec2 describe-images --filters "Name=name,Values='${IMAGE_PATTERN}'" --owners amazon --query "reverse(sort_by(Images, &CreationDate))[0].ImageId" --output text)
+  if [ "z${IMAGE_OWNER}" != "z" ]; then
+    OWNER_ARG="--owners ${DEFAULT_IMAGE_OWNER}"
+  fi
+  IMAGE_ID=$(aws ec2 describe-images --filters "Name=name,Values='${IMAGE_PATTERN}'" ${OWNER_ARG} --query "reverse(sort_by(Images, &CreationDate))[0].ImageId" --output text)
   echo "${IMAGE_ID}"
 }
 
